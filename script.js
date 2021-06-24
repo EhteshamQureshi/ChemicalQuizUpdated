0f82fdab8fcefc54e2d032608caefdd513e53804
Ehtesham Qureshi <ehteshamqureshi.101@gmail.com>
2021-06-24 10:26:05 +0500

    Chemical quiz. Randomly coming elements with 3 difficulty levels and scoring

---
+++ script.js
@@ -0,0 +1,110 @@
+function easy() {
+  let v = document.getElementById("e1").value;
+  let totalScore = 0;
+  if (v == "H") {
+    ++totalScore;
+  }
+  v = document.getElementById("e2").value;
+  if (v == "He") {
+    ++totalScore;
+  }
+  v = document.getElementById("e3").value;
+  if (v == "Li") {
+    ++totalScore;
+  }
+  v = document.getElementById("e4").value;
+  if (v == "Be") {
+    ++totalScore;
+  }
+  let score = document.getElementById("score");
+  score.value = "Your Score = " + totalScore;
+}
+function medium() {
+  let v = document.getElementById("e1").value;
+  let totalScore = 0;
+  if (v == "H") {
+    ++totalScore;
+  }
+  v = document.getElementById("e2").value;
+  if (v == "He") {
+    ++totalScore;
+  }
+  v = document.getElementById("e3").value;
+  if (v == "Li") {
+    ++totalScore;
+  }
+  v = document.getElementById("e4").value;
+  if (v == "Be") {
+    ++totalScore;
+  }
+  if (v == "B") {
+    ++totalScore;
+  }
+  v = document.getElementById("e2").value;
+  if (v == "C") {
+    ++totalScore;
+  }
+  v = document.getElementById("e3").value;
+  if (v == "N") {
+    ++totalScore;
+  }
+  v = document.getElementById("e4").value;
+  if (v == "O") {
+    ++totalScore;
+  }
+  let score = document.getElementById("score");
+  score.value = "Your Score = " + totalScore;
+}
+function hard() {
+  let v = document.getElementById("e1").value;
+  let totalScore = 0;
+  if (v == "H") {
+    ++totalScore;
+  }
+  v = document.getElementById("e2").value;
+  if (v == "He") {
+    ++totalScore;
+  }
+  v = document.getElementById("e3").value;
+  if (v == "Li") {
+    ++totalScore;
+  }
+  v = document.getElementById("e4").value;
+  if (v == "Be") {
+    ++totalScore;
+  }
+  v = document.getElementById("e5").value;
+  if (v == "B") {
+    ++totalScore;
+  }
+  v = document.getElementById("e6").value;
+  if (v == "C") {
+    ++totalScore;
+  }
+  v = document.getElementById("e7").value;
+  if (v == "N") {
+    ++totalScore;
+  }
+  v = document.getElementById("e8").value;
+  if (v == "O") {
+    ++totalScore;
+  }
+  v = document.getElementById("e9").value;
+  if (v == "F") {
+    ++totalScore;
+  }
+  v = document.getElementById("e10").value;
+  if (v == "Ne") {
+    ++totalScore;
+  }
+  v = document.getElementById("e11").value;
+  if (v == "Na") {
+    ++totalScore;
+  }
+  v = document.getElementById("e12").value;
+  if (v == "Mg") {
+    ++totalScore;
+  }
+  let score = document.getElementById("score");
+  score.value = "Your Score = " + totalScore;
+}
